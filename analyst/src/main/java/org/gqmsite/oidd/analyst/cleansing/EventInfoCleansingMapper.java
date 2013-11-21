c80ff711786d21fb68360a81263d76e9eaf1026e
Quentin <qiuming.guo@gmail.com>
2013-11-21 12:03:25 +0800

    add files

--- analyst/src/main/java/org/gqmsite/oidd/analyst/cleansing/EventInfoCleansingMapper.java
+++ analyst/src/main/java/org/gqmsite/oidd/analyst/cleansing/EventInfoCleansingMapper.java
@@ -1,5 +1,54 @@
 package org.gqmsite.oidd.analyst.cleansing;
 
-public class EventInfoCleansingMapper {
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class EventInfoCleansingMapper extends
+		Mapper<LongWritable, Text, Text, EventInfo> {
+
+	private EventInfo info = new EventInfo();
+
+	@Override
+	protected void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		StringTokenizer itr = new StringTokenizer(value.toString(), ",");
+		String temp;
+		int index = 0;
+
+		while (itr.hasMoreElements()) {
+			if (index == 0) {
+				info.getCell().set(itr.nextToken());
+			} else if (index == 1) {
+				temp = itr.nextToken();
+				if (StringUtils.isNumeric(temp)) {
+					info.getSector().set(Integer.parseInt(temp));
+				} else {
+					// maybe add a counter to count the invalid elements
+					break;
+				}
+			} else if (index == 4) {
+				info.getMdn().set(itr.nextToken());
+			} else if (index == 6) {
+				info.getTrackTime().set(itr.nextToken());
+			} else if (index == 7) {
+				temp = itr.nextToken();
+				if (StringUtils.isNumeric(temp)) {
+					info.getEvent().set(Integer.parseInt(temp));
+				} else {
+					// maybe add a counter to count the invalid elements
+					break;
+				}
+				context.write(info.getTrackTime(), info);
+			} else {
+				itr.nextToken();
+			}
+			index++;
+		}
+	}
 
 }
