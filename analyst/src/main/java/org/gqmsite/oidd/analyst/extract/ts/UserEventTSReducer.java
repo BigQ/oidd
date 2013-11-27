fb56f38881424f81b6aa395e677172addb0ae70c
GQM <18901850925@189.cn>
2013-11-27 15:35:22 +0800

    fix reducer

--- analyst/src/main/java/org/gqmsite/oidd/analyst/extract/ts/UserEventTSReducer.java
+++ analyst/src/main/java/org/gqmsite/oidd/analyst/extract/ts/UserEventTSReducer.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.gqmsite.oidd.analyst.io.EventArray;
 import org.gqmsite.oidd.analyst.io.EventInfo;
@@ -13,18 +14,23 @@ import org.gqmsite.oidd.analyst.io.UserTimePair;
 public class UserEventTSReducer extends
 		Reducer<UserTimePair, EventInfo, Text, EventArray> {
 
-	private String preMdn = null;
+	private String preMDN = null;
 	private EventArray array = new EventArray();
 	private List<EventInfo> container = new ArrayList<EventInfo>();
 
 	@Override
 	protected void reduce(UserTimePair key, Iterable<EventInfo> values,
 			Context context) throws IOException, InterruptedException {
-		if (preMdn == null) {
-			preMdn = key.getMdn().toString();
-		} else if (!preMdn.equals(key.getMdn().toString())) {
-			preMdn = key.getMdn().toString();
-			context.write(key.getMdn(), array);
+		if (preMDN == null) {
+			// first invoke
+			preMDN = key.getMdn().toString();
+		} else if (!preMDN.equals(key.getMdn().toString())) {
+			// flush the pre-MDN data
+			flushData(context);
+			// clean the container
+			container.clear();
+			// mark the new MDN
+			preMDN = key.getMdn().toString();
 		}
 
 		for (EventInfo info : values) {
@@ -35,7 +41,14 @@ public class UserEventTSReducer extends
 	@Override
 	protected void cleanup(Context context) throws IOException,
 			InterruptedException {
-		context.write(new Text(preMdn), array);
+		// flush the pre-MDN data
+		flushData(context);
 	}
 
+	private void flushData(Context context) throws IOException,
+			InterruptedException {
+		Writable[] arrtemp = new Writable[container.size()];
+		array.set(container.toArray(arrtemp));
+		context.write(new Text(preMDN), array);
+	}
 }
