05f11ceba41adecf2de554eeaaa5acec5c02d526
GQM <18901850925@189.cn>
2014-02-26 09:45:50 +0800

    fix

--- connector/src/main/java/org/gqmsite/oidd/connector/ai/AiEventLoadMapper.java
+++ connector/src/main/java/org/gqmsite/oidd/connector/ai/AiEventLoadMapper.java
@@ -7,7 +7,8 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.gqmsite.oidd.connector.avro.schema.Event;
+import org.gqmsite.oidd.connector.schema.Event;
+
 
 public class AiEventLoadMapper extends
 		Mapper<LongWritable, Text, AvroKey<Event>, NullWritable> {
