786a417c3d03ab8203b4b33bfc038b0774662a0c
GQM <18901850925@189.cn>
2014-02-27 09:04:02 +0800

    fix

--- analyst/src/main/java/com/sanss/oidd/analyst/ts/EventTSPartitioner.java
+++ analyst/src/main/java/com/sanss/oidd/analyst/ts/EventTSPartitioner.java
@@ -9,7 +9,7 @@ public class EventTSPartitioner extends Partitioner<UserTimePair, EventInfo> {
 
 	@Override
 	public int getPartition(UserTimePair key, EventInfo value, int numPartitions) {
-		return Math.abs(key.getUser().hashCode()) % numPartitions;
+		return Math.abs(key.getUser().toString().hashCode()) % numPartitions;
 	}
 
 }
