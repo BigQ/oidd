70a989f90d48018016a01e4e0e6717fdd78235ab
GQM <18901850925@189.cn>
2014-03-04 15:58:47 +0800

    enhance MapFileGet

--- common/src/main/java/com/sanss/oidd/common/utils/MapFileGet.java
+++ common/src/main/java/com/sanss/oidd/common/utils/MapFileGet.java
@@ -1,6 +1,9 @@
 package com.sanss.oidd.common.utils;
 
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.MapFile;
 import org.apache.hadoop.io.Text;
@@ -25,24 +28,29 @@ public class MapFileGet extends Configured implements Tool {
 		}
 
 		String mapUri = args[0];
-
-		Path map = new Path(mapUri);
-		try (MapFile.Reader reader = new MapFile.Reader(map, getConf())) {
-			Text mdn = new Text(args[1]);
-			// new instance
-			Writable value = (Writable) reader.getValueClass().newInstance();
-			// read value
-			if (reader.get(mdn, value) != null) {
-				// print value
-				System.out.println(value.toString());
-				while (reader.next(mdn, value)) {
-					if (!mdn.toString().endsWith(args[1])) {
-						break;
-					}
+		FileSystem fs = FileSystem.get(getConf());
+		Path path = new Path(mapUri);
+		FileStatus[] status = fs.globStatus(path);
+		Path[] listPaths = FileUtil.stat2Paths(status);
+		for (Path map : listPaths) {
+			try (MapFile.Reader reader = new MapFile.Reader(map, getConf())) {
+				Text mdn = new Text(args[1]);
+				// new instance
+				Writable value = (Writable) reader.getValueClass()
+						.newInstance();
+				// read value
+				if (reader.get(mdn, value) != null) {
+					// print value
 					System.out.println(value.toString());
+					while (reader.next(mdn, value)) {
+						if (!mdn.toString().endsWith(args[1])) {
+							break;
+						}
+						System.out.println(value.toString());
+					}
 				}
-			}
 
+			}
 		}
 		return 0;
 	}
